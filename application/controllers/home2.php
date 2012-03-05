50975153ab1333a0af53f5323fdf71883415b495
Elise Koster <koster.elise@gmail.com>
2012-03-05 13:41:56 -0500

    herp

--- application/controllers/home2.php
+++ application/controllers/home2.php
@@ -4,7 +4,7 @@ class Home2 extends CI_Controller
 {
     public function index() 
     {
-        $this->load->model('Home');
+        $this->load->model('Model');
         $posts = $this->Home->get_latest();
         #var_dump($posts);
         $this->load->view('home2/index.php', array('posts' => $posts));
