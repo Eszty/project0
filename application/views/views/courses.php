ffdeac25bd9fa6e4b71de66efa964a3d1832defa
Eszter Fodor <chytos@gmail.com>
2012-03-07 04:39:16 -0500

    fixing

--- application/views/views/courses.php
+++ application/views/views/courses.php
@@ -6,15 +6,10 @@
     <body>
         <?php
             foreach ($posts as $post): ?>
-<<<<<<< HEAD
-            <p><?php echo $post->id; ?></p>
-            <p><?php echo $post->title; ?></p>
-=======
             <p><?php echo $post->cat_num; ?></p>
             <p><?php echo $post->title; ?></p>
             <p><?php echo $post->description; ?></p>
             <p><?php echo $post->instructor; ?></p>
->>>>>>> 4793a49f79c2f65dc7526ab5b5ace71c2c86d7be
         <?php endforeach; ?>
     </body>
 </html>
