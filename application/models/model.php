426ac3e5def8d502fcef5e8b62cd7a6de79b5853
Eszter Fodor <chytos@gmail.com>
2012-03-07 04:31:19 -0500

    fixing corrupted files

--- application/models/model.php
+++ application/models/model.php
@@ -6,11 +6,7 @@ class Model extends CI_Model {
     # Function to get the content of the table 'courses'
 	public function get_courses() 
 	{
-<<<<<<< HEAD
-    	return $this->db->order_by('title', 'desc')->get('courses')->result();
-=======
     	return $this->db->order_by('title', 'asc')->get('courses')->result();
->>>>>>> 4793a49f79c2f65dc7526ab5b5ace71c2c86d7be
 	}
 	
 	public function get_users()
@@ -20,17 +16,22 @@ class Model extends CI_Model {
     
     public function get_taking()
     {
-<<<<<<< HEAD
-        return $this->db->get('taking')->result();
-=======
-        $user_id = 3;
+        $user_id = 2;
+        $this->db->select('course_id');
         $c_id= $this->db->get_where('taking', array('user_id'=>$user_id))->result();
-        var_dump($c_id);
+        #var_dump($c_id);
+        
+        $array=array();
         foreach ($c_id as $cor_id)
         {
-           return $this->db->get_where('courses', array('course_id'=>$cor_id))->result();
+           #var_dump($cor_id);
+           #var_dump($cor_id);
+           $a= $this->db->get_where('courses', array('id'=>($cor_id->course_id)))->result();
+           array_push($array,$a);
+           #var_dump($array);
         }
->>>>>>> 4793a49f79c2f65dc7526ab5b5ace71c2c86d7be
+        #var_dump($array);
+        return $array;
     }
     
     public function get_shopping()
@@ -40,12 +41,8 @@ class Model extends CI_Model {
     
     public function get_faculty()
     {
-<<<<<<< HEAD
-        return $this->db->get('faculty')->result();
-=======
         $dep = "MUSC";
         return $this->db->get_where('courses', array('dep_code'=>$dep))->result();
->>>>>>> 4793a49f79c2f65dc7526ab5b5ace71c2c86d7be
 	}
 	
 }
