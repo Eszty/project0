324962a8baeebe9e1a9195b44397b10cc71a8552
Eszter Fodor <chytos@gmail.com>
2012-03-09 16:05:30 -0500

    work in progress, busy with: adding courses

--- application/views/views/courses.php
+++ application/views/views/courses.php
@@ -8,8 +8,10 @@
             $this->load->helper('form');
             foreach ($posts as $post): 
                 foreach ($post as $input): ?>
-                <p><?php echo form_checkbox('newsletter', 'accept', False); echo(' Shop '); 
-                echo form_checkbox('newsletter', 'accept', False); echo(' Take'); ?></p>              
+                <!--<p><?php echo form_checkbox('newsletter', 'accept', False); echo(' Shop '); 
+                echo form_checkbox('newsletter', 'accept', False); echo(' Take'); ?></p> -->
+                <input type="checkbox" name="options[]" value="shop" /> Shop <br> 
+                <input type="checkbox" name="options[]" value="take" /> Take <p>           
                 <p><?php echo $input->cat_num; ?></p>
                 <p><?php echo $input->title; ?></p>
                 <p><?php echo $input->description; ?></p>
